a3aa192373df4381ec14319785b74eeec2008204
Akshaytr <akshaytr31@gmail.com>
2025-06-15 20:01:25 +0530

    initial

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+
+
+    <!-- header -->
+
+    <header>
+      <h1>Todo list</h1>
+    </header>
+    
+  </main>
+</template>
+
+<script>
+  export default {
+    
+
+  }
+</script>
