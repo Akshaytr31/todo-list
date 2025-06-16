e328229d524e84bdbd3e8d84eef2ef4bfbd73a2b
Akshaytr <akshaytr31@gmail.com>
2025-06-16 15:33:54 +0530

    feat:add addList and delete list

--- src/stores/ListStore.js
+++ src/stores/ListStore.js
@@ -8,5 +8,15 @@ export const useListStore=defineStore('listStore',{
                 id:1,title:'list one'
             }
         ],
-    })
+    }),
+    actions:{
+        addTask(list){
+            this.Lists.push(list)
+        },
+        deleteTask(id){
+            this.list=this.Lists.filter(t=>{
+                return t.id!==id
+            })
+        }
+    }
 })
