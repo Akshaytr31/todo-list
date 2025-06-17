1786a481901c8354ea72c9a3006eb4943c57d6db
Akshaytr <akshaytr31@gmail.com>
2025-06-17 19:06:43 +0530

    feat:remove hard coded data

--- src/stores/ListStore.js
+++ src/stores/ListStore.js
@@ -3,7 +3,12 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export const useListStore = defineStore("listStore", () => {
-  const Lists = ref([{ id: 1, title: "list one" }]);
+  const Lists = ref([]);
+  const getTask = async () => {
+    const res = await fetch("http://localhost:3000/tasks");
+    const data = await res.json();
+    Lists.value = data;
+  };
 
-  return { Lists };
+  return { Lists,getTask };
 });
