8e3b5b72e263a103099decd587838df813b1d6c8
Akshaytr <akshaytr31@gmail.com>
2025-06-18 09:17:46 +0530

    feat:remove fetch api from the store

--- src/stores/ListStore.js
+++ src/stores/ListStore.js
@@ -3,11 +3,6 @@ import { ref } from "vue";
 
 export const useListStore = defineStore("listStore", () => {
   const Lists = ref([]);
-  const getTask = async () => {
-    const res = await fetch("http://localhost:3000/tasks");
-    const data = await res.json();
-    Lists.value = data;
-  };
 
-  return { Lists,getTask };
+  return { Lists};
 });
