1f61ec4ce72c7c89b6bc551ca4189a56816defab
Akshaytr <akshaytr31@gmail.com>
2025-06-18 13:33:29 +0530

    feat:add new button component

---
+++ src/components/BaseButton.vue
@@ -0,0 +1,35 @@
+<template>
+    <button class="['base-button',variant]">
+        {{ icon }}
+    </button>
+</template>
+
+<script>
+    export default {
+        name:'BaseButton',
+        variant:{
+            type:String,
+            default:'primary',
+        },
+        icon:{
+            add:'Add',
+            view:'View',
+            delete:'Delete'
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .base-button{
+        padding: 10px;
+        border: none;
+        border-radius: 8px;
+        font-size: 18px;
+        cursor: pointer;
+        transition: 0.3s ease all;
+    }
+    .base-button.primary{
+        background-color: #007bff;
+        color: #fff;
+    }
+</style>
