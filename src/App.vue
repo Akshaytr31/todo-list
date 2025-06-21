649064e65dfcea0e3714ba92a18e5433aba6621d
Akshaytr <akshaytr31@gmail.com>
2025-06-21 13:32:34 +0530

    feat: add new componet task list

--- src/App.vue
+++ src/App.vue
@@ -10,18 +10,16 @@
     <!-- input form -->
     <InputForm/>
 
+    <TaskList/>
+    
 
-    <div class="list-elements">
-      <div v-for="list in listStore.Lists" :key="list.id">
-        <ListElement :list="list"/>
-      </div>
-    </div>
+
   </main>
 </template>
 
 <script setup>
+import TaskList from "./components/TaskList.vue";
 import InputForm from "./components/InputForm.vue";
-import ListElement from "./components/ListElement.vue";
 import { useListStore } from "./stores/ListStore";
 
   const listStore=useListStore()
---
+++ src/components/TaskList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="list-elements">
+    <div v-for="list in listStore.lists" :key="list.id">
+      <ListElement :list="list" />
+    </div>
+    <div class="clear-block">
+      <div class="number-task">
+        <label for="">Total number of Tasks : </label>
+        <span class="count-tasks">{{ listStore.lists.length }}</span>
+      </div>
+      <BaseButtons variant="clear" text="Clear All" @clear-event="clearAll" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useListStore } from '@/stores/ListStore';
+import BaseButtons from './BaseButtons.vue';
+import ListElement from './ListElement.vue';
+
+const listStore = useListStore();
+
+const clearAll = () => {
+  listStore.lists = [];
+};
+</script>
+
+<style lang="scss" scoped>
+.clear-block{
+    display: flex;
+    align-items: flex-end;
+    justify-content: space-between;
+}
+
+.count-tasks{
+    padding:5px 10px;
+    background: rgb(95, 194, 243);
+    color: #fff;
+    border-radius: 10px;
+}
+</style>
