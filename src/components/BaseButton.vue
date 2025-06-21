aba5f94e7474b1516933395986701335016d3c58
Akshaytr <akshaytr31@gmail.com>
2025-06-21 12:10:14 +0530

    feat: Add clear emit function

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,68 +1,65 @@
 <template>
-  <button :class="['base-button',variant]">
-      <i v-if="icon" class="material-icons" @click="$emit('deleteEvent')">{{ icon }}</i>
-      <span>{{ text }}</span>
+  <button :class="['base-button', variant]">
+    <i v-if="icon" class="material-icons" @click="$emit('deleteEvent')">
+      {{ icon }}
+    </i>
+    <span @click="$emit('clearEvent')">{{ text }}</span>
   </button>
 </template>
 
 <script setup>
-  defineProps({
-      text:String,
-      variant:{
-          type:String,
-          default:'primary'
-      },
-      icon:String
-  })
+defineProps({
+  text: String,
+  variant: {
+    type: String,
+    default: "primary",
+  },
+  icon: String,
+});
 </script>
 
 <style lang="scss" scoped>
 .base-button {
-padding: 10px;
-border: none;
-border-radius: 8px;
-font-size: 18px;
-cursor: pointer;
-transition: 0.3s ease all;
-display: flex;
-align-items: center;
-gap: 8px;
-display: flex;
-justify-content: center;
-align-items: center;
+  padding: 10px;
+  border: none;
+  border-radius: 8px;
+  font-size: 18px;
+  cursor: pointer;
+  transition: 0.3s ease all;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.base-button i{
-padding-left: 8px;
+.base-button i {
+  padding-left: 8px;
 }
 
 .base-button.primary {
-background-color: green;
-color: #fff;
+  background-color: green;
+  color: #fff;
 }
 
 .base-button.view {
-background-color: #007bff;
-color: #fff;
+  background-color: #007bff;
+  color: #fff;
 }
 
 .base-button.delete {
-background-color: red;
-color: #fff;
+  background-color: red;
+  color: #fff;
 }
 
 .base-button.clear {
-background-color: rgb(255, 132, 0);
-color: #fff;
-margin-top: 20px;
+  background-color: rgb(255, 132, 0);
+  color: #fff;
+  margin-top: 20px;
 }
 
-.clear-block{
-display: flex;
-}
-
-.number-task{
-display: flex;
-gap: 10px;
+.clear-block {
+  display: flex;
 }
 </style>
