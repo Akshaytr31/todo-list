9baee5480d678b4643d75b52385072a78059b171
Akshaytr <akshaytr31@gmail.com>
2025-06-23 12:32:43 +0530

    feat: add function to emit

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,69 +1,79 @@
 <template>
-  <button :class="['base-button',variant]">
-      <i v-if="icon" class="material-icons" @click="$emit('deleteEvent')">{{ icon }}</i>
-      <span @click="$emit('clearEvent')">{{ text }}</span>
-      <span @click="$emit('editEvent')">{{ text }}</span>
-  </button >
+  <button :class="['base-button', variant]">
+    <i v-if="icon" class="material-icons" @click="handleEvent">{{ icon }}</i>
+    <span @click="$emit('clearEvent')">{{ text }}</span>
+  </button>
 </template>
-
 <script setup>
-  defineProps({
-      text:String,
-      variant:{
-          type:String,
-          default:'primary'
-      },
-      icon:String
-  })
-</script>
+import { defineEmits } from "vue";
 
+defineProps({
+  text: String,
+  variant: {
+    type: String,
+    default: "primary",
+  },
+  icon: String,
+});
+
+const emit = defineEmits();
+
+const handleEvent = () => {
+  emit("onClick");
+};
+</script>
 <style lang="scss" scoped>
 .base-button {
-padding: 10px;
-border: none;
-border-radius: 8px;
-font-size: 18px;
-cursor: pointer;
-transition: 0.3s ease all;
-display: flex;
-align-items: center;
-gap: 8px;
-display: flex;
-justify-content: center;
-align-items: center;
+  padding: 10px;
+  border: none;
+  border-radius: 8px;
+  font-size: 18px;
+  cursor: pointer;
+  transition: 0.3s ease all;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
-.base-button i{
-padding-left: 8px;
+.base-button i {
+  padding-left: 8px;
 }
 
 .base-button.primary {
-background-color: green;
-color: rgb(255, 255, 255);
+  background-color: green;
+  color: rgb(255, 255, 255);
 }
 
 .base-button.view {
-background-color:  rgb(0, 123, 255);
-color: rgb(255, 255, 255);
+  background-color: rgb(0, 123, 255);
+  color: rgb(255, 255, 255);
 }
 
 .base-button.delete {
-background-color: rgb(255, 0, 0);
-color: rgb(255, 255, 255);
+  background-color: rgb(255, 0, 0);
+  color: rgb(255, 255, 255);
 }
 
 .base-button.clear {
-background-color: rgb(255, 132, 0);
-color: rgb(255, 255, 255);
-margin-top: 20px;
+  background-color: rgb(255, 132, 0);
+  color: rgb(255, 255, 255);
+  margin-top: 20px;
 }
 
-.base-button.edit{
+.base-button.edit {
   background-color: rgb(0, 183, 255);
-  color:rgb(255, 255, 255);
+  color: rgb(255, 255, 255);
 }
 
-.clear-block{
-display: flex;
+.base-button.home {
+  background-color: rgb(47, 47, 47);
+  color: rgb(255, 255, 255);
+}
+
+.clear-block {
+  display: flex;
 }
 </style>
