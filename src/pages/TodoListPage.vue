d4db8783c8e9514d2715f7b43acea062d5cf2154
Akshaytr <akshaytr31@gmail.com>
2025-06-25 10:44:53 +0530

    feat: TodoListPage

---
+++ src/pages/TodoListPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <main>
+    <header>
+      <h1>{{ navHeading.title }}</h1>
+    </header>
+    <InputForm />
+    <TaskList />
+    <DeleteItem />
+  </main>
+</template>
+<script setup>
+import DeleteItem from "@/components/DeleteItem.vue";
+import TaskList from "@/components/TaskList.vue";
+import InputForm from "@/components/TaskForm.vue";
+
+const navHeading = {
+  title: "Todo List",
+};
+</script>
