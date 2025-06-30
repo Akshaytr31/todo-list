c448d9348a7be5d738556b083071aa90967b953b
Akshaytr <akshaytr31@gmail.com>
2025-06-30 09:44:31 +0530

    feat:Remove delete component

--- src/pages/TodoListPage.vue
+++ src/pages/TodoListPage.vue
@@ -2,11 +2,9 @@
   <main>
     <TaskForm />
     <TaskList />
-    <DeleteItem />
   </main>
 </template>
 <script setup>
-import DeleteItem from "@/components/DeleteItem.vue";
 import TaskList from "@/components/TaskList.vue";
 import TaskForm from "@/components/TaskForm.vue";
 </script>
