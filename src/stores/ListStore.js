f55780b325ae2c5c6614dcb448dccc6967cdfebe
Akshaytr <akshaytr31@gmail.com>
2025-07-01 12:19:21 +0530

    feat: add update function

--- src/stores/ListStore.js
+++ src/stores/ListStore.js
@@ -1,16 +1,27 @@
-import { defineStore } from "pinia";
 import { ref } from "vue";
+import { defineStore } from "pinia";
 
-export const useListStore = defineStore("listStore", () => {
-  const lists = ref([]);
+export const useListStore = defineStore(
+  "listStore",
+  () => {
+    const lists = ref([]);
 
-  const setTodoList = (data) => {
-    lists.value.push(data)
-  };
+    const setTodoList = (data) => {
+      lists.value.push(data);
+    };
 
-  const deleteList=(id)=>{
-    lists.value = lists.value.filter(item=>(item.id !== id));
+    const deleteList = (id) => {
+      lists.value = lists.value.filter((item) => item.id !== id);
+    };
+
+    const updateTodolist = (updatedTask) => {
+      const index = lists.value.findIndex((item) => item.id === updatedTask.id);
+      lists.value[index] = { ...lists.value[index], ...updatedTask };
+    };
+
+    return { lists, setTodoList, deleteList, updateTodolist};
+  },
+  {
+    persist: true,
   }
-  
-  return { lists, setTodoList ,deleteList}
-});
+);
