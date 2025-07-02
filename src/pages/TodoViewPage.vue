01a062c2062028f7c690fd2fc25e8fd86d1e406c
Akshaytr <akshaytr31@gmail.com>
2025-07-02 11:21:29 +0530

    feat: add user features

--- src/pages/TodoViewPage.vue
+++ src/pages/TodoViewPage.vue
@@ -1,21 +1,29 @@
 <template>
-  <TaskForm v-model:newTask="newTask" v-model:status="status" :showAddButton="false">
-    <template #back>
-      <div class="back-button">
-        <router-link to="/" style="text-decoration: none">
-          <BaseButtons variant="back" text="back" />
-        </router-link>
-      </div>
-    </template>
-    <template #edit>
-      <div class="edit-button">
-        <BaseButtons variant="edit" text="Edit Task" @on-click="updateTodoItem"/>
-      </div>
-    </template>
-  </TaskForm>
+  <div class="wrapper">
+    <div class="back-button">
+      <router-link to="/" style="text-decoration: none">
+        <BaseButtons variant="back" text="back" />
+      </router-link>
+    </div>
+    <TaskForm
+      v-model:newTask="newTask"
+      v-model:status="status"
+      v-model:assignedUser="assignedUser"
+    >
+      <template #footer>
+        <div class="edit-button">
+          <BaseButtons
+            variant="edit"
+            text="Edit Task"
+            @on-click="updateTodoItem"
+          />
+        </div>
+      </template>
+    </TaskForm>
+  </div>
 </template>
 <script setup>
-import { ref, onMounted } from "vue";
+import { ref } from "vue";
 import { useRoute, useRouter } from "vue-router";
 
 import { useListStore } from "@/stores/ListStore";
@@ -26,31 +34,51 @@ const route = useRoute();
 
 const router = useRouter();
 
-const taskId = route.params.id;
+const taskID = route.params.id;
 
 const listStore = useListStore();
 
 const newTask = ref("");
+
 const status = ref("");
+
+const assignedUser = ref(null);
 
 const updateTodoItem = () => {
   if (newTask.value.length > 0) {
     listStore.updateTodolist({
       title: newTask.value,
-      status: status,
-      id: Number(taskId),
+      status: status.value,
+      assignedUser: assignedUser.value,
+      id: Number(taskID),
     });
     router.push("/");
   }
 };
+import { watchEffect } from "vue";
 
-onMounted(() => {
-  const task = listStore.lists.find((item) => item.id === Number(taskId));
-  newTask.value = task.title;
-  status.value = task.status;
+watchEffect(() => {
+  const task = listStore.lists.find((item) => item.id === Number(taskID));
+  if (task) {
+    newTask.value = task.title;
+    status.value = task.status;
+    assignedUser.value = task.assignedUser || null;
+  } else {
+    console.log("There is no task");
+  }
 });
 </script>
 <style lang="scss" scoped>
+.wrapper {
+  max-width: 700px;
+  margin: 0 auto;
+  position: relative;
+}
+
+.main-container {
+  padding: 80px 0 40px;
+}
+
 .edit-button {
   display: flex;
   justify-content: flex-end;
@@ -58,6 +86,8 @@ onMounted(() => {
 }
 
 .back-button {
-  padding: 0 20px 20px;
+  position: absolute;
+  padding: 10px;
+  top: 0;
 }
 </style>
