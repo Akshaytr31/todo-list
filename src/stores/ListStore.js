0d9b08c7a8c12f3ca3014afead6dffc45edafcb4
Akshaytr <akshaytr31@gmail.com>
2025-07-02 14:13:21 +0530

    feat: add axios to fetch data

--- src/stores/ListStore.js
+++ src/stores/ListStore.js
@@ -1,35 +1,38 @@
-import { ref } from 'vue'
-import { defineStore } from 'pinia'
+import { ref } from "vue";
+import { defineStore } from "pinia";
+import axios from "axios";
+
+import API_URL from "../routes/index";
 
 export const useListStore = defineStore(
-  'listStore',
+  "listStore",
   () => {
-    const lists = ref([])
-    const users = ref([])
+    const lists = ref([]);
+    const users = ref([]);
 
     const setTodoList = (data) => {
-      lists.value.push(data)
-    }
+      lists.value.push(data);
+    };
 
     const deleteList = (id) => {
-      lists.value = lists.value.filter((item) => item.id !== id)
-    }
+      lists.value = lists.value.filter((item) => item.id !== id);
+    };
 
     const updateTodolist = (updatedTodoItem) => {
       lists.value = lists.value.map((item) =>
         item.id === updatedTodoItem.id ? { ...item, ...updatedTodoItem } : item
-      )
-    }
+      );
+    };
 
     const fetchUsers = async () => {
       try {
-        const res = await fetch('https://jsonplaceholder.typicode.com/users')
-        const data = await res.json()
-        users.value = data
+        const res = await axios.get(API_URL);
+        const data = await res.json();
+        users.value = data;
       } catch (err) {
-        console.error('Failed to fetch users:', err)
+        console.error("Failed to fetch users:", err);
       }
-    }
+    };
 
     return {
       lists,
@@ -37,10 +40,10 @@ export const useListStore = defineStore(
       setTodoList,
       deleteList,
       updateTodolist,
-      fetchUsers
-    }
+      fetchUsers,
+    };
   },
   {
-    persist: true
+    persist: true,
   }
-)
+);
