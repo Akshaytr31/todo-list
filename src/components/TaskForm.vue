0d8cd974aa9a313bcaa77887f77ff62f06698d67
Akshaytr <akshaytr31@gmail.com>
2025-07-02 10:46:06 +0530

    feat: add user drop down component

--- src/components/TaskForm.vue
+++ src/components/TaskForm.vue
@@ -4,50 +4,110 @@
       <div class="form-container">
         <div class="input-container">
           <label for="input">Todo item:</label>
-          <input type="text" placeholder="Enter list item" v-model="newTask"/>
+          <input
+            type="text"
+            placeholder="Enter list item"
+            @input="updateNewTask"
+            v-model="newTask"
+          />
         </div>
         <div class="input-container">
           <label for="type">Status:</label>
           <div class="select-container">
-            <select name="type" id="type" v-model="form.status">
-              <option value="">-Select status-</option>
+            <select
+              name="type"
+              id="type"
+              @change="updateStatus"
+              v-model="form.status"
+            >
+              <option value="-">-Select status-</option>
               <option value="Todo">Todo</option>
               <option value="In Progress">In Progress</option>
               <option value="Done">Done</option>
             </select>
           </div>
         </div>
+        <div class="input-container">
+          <label for="user">Assigned to:</label>
+          <UserDropDown
+            :users="listStore.users"
+            :modelValue="assignedUser"
+            @update:modelValue="onUserSelect"
+            class="userSelction"
+          />
+        </div>
       </div>
-      <div class="button-container">
-        <BaseButtons variant="primary" text="Add Task" />
-      </div>
+      <slot name="footer"></slot>
     </form>
   </div>
 </template>
 <script setup>
-import { reactive, ref } from "vue";
-
+import { ref, reactive, onMounted, watch } from "vue";
 import { useListStore } from "@/stores/ListStore";
-import BaseButtons from "./BaseButtons.vue";
+import UserDropDown from "./UserDropDown.vue";
 
-const form = reactive({
-  status: "",
+const props = defineProps({
+  newTask: String,
+  status: String,
+  assignedUser: Object,
 });
+
+const emit = defineEmits([
+  "update:newTask",
+  "update:status",
+  "update:assignedUser",
+]);
+
+const onUserSelect = (val) => {
+  assignedUser.value = val;
+  emit("update:assignedUser", val);
+};
+
+const newTask = ref("");
+
+const form = reactive({ status: "-" });
+
+const assignedUser = ref(null);
 
 const listStore = useListStore();
 
-const newTask = ref("");
+
+const updateNewTask = (event) => {
+  emit("update:newTask", event.target.value);
+};
+
+const updateStatus = (event) => {
+  emit("update:status", event.target.value);
+};
 
 const addTodo = () => {
   if (newTask.value.length > 0) {
     listStore.setTodoList({
       title: newTask.value,
       status: form.status,
+      assignedUser: assignedUser.value,
       id: listStore.lists.length + 1,
     });
+    
     newTask.value = "";
+    form.status = "-";
+    assignedUser.value = "";
   }
 };
+
+onMounted(() => {
+  if (listStore.users.length === 0) {
+    listStore.fetchUsers();
+  }
+});
+
+watch(
+  () => props.assignedUser,
+  (val) => {
+    assignedUser.value = val;
+  },
+  { immediate: true }
+);
 </script>
 <style lang="scss" scoped>
 .main-container {
@@ -81,15 +141,22 @@ const addTodo = () => {
   text-wrap: nowrap;
 }
 
-.input-container:last-child {
+.userSelction {
+  width: 100%;
+}
+
+.input-container:nth-child(2) {
   gap: 80px;
+}
+
+.input-container:last-child {
+  gap: 38px;
 }
 
 .select-container {
   width: 100%;
   box-shadow: 2px 4px 6px rgba(0, 0, 0, 0.05);
-  padding: 7px 10px;
-  border-radius: 10px;
+  border-radius: 5px;
   border: 1px solid rgb(232, 232, 232);
 }
 
@@ -101,8 +168,8 @@ const addTodo = () => {
 }
 
 input {
-  padding: 10px;
-  border-radius: 10px;
+  padding: 8px;
+  border-radius: 5px;
   border: none;
   box-shadow: 2px 4px 6px rgba(0, 0, 0, 0.05);
   width: 100%;
@@ -117,6 +184,8 @@ select {
   border: none;
   width: 100%;
   background: rgb(255, 255, 255);
+  padding: 7px;
+  border-radius: 5px;
 }
 
 select:focus {
